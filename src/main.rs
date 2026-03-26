8878fb7db255471d589b8df176b2693f04783a87
fengqi <362254883@qq.com>
2026-03-26 22:28:01 +0800

    chore: rename AGENT.md to AGENTS.md and simplify use statement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use custom_utils::logger;
-use log::LevelFilter::{Info};
+use log::LevelFilter::Info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let _ = logger::logger_feature("app", "debug", Info, false).build();
-
+    // remember to print msg via stdio
     rust_template::run().await
 }
